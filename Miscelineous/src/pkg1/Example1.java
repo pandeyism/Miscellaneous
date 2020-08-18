930a8e9350f3e16e8579b61ed9177c18bfc36ff4
sanjeev <sanjeev@sanjeev-PC>
2020-08-18 12:47:19 +0530

    Pushing the code first time

---
+++ Miscelineous/src/pkg1/Example1.java
@@ -0,0 +1,13 @@
+package pkg1;
+
+public class Example1  
+{
+    public static void main(String [] args) 
+    {
+        int x=20;
+        String sup = (x < 15) ? "small" : (x < 22)? "tiny" : "huge";
+        System.out.println(sup);
+    }
+}
+
+
